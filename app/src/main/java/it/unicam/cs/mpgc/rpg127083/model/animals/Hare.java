2a98bc82b321af4c0f7eddb70277fb1ea4e4ecac
Chiara Marinucci <chiaramarinucci94@gmail.com>
2026-06-28 19:54:17 +0200

    removed Nest class

--- app/src/main/java/it/unicam/cs/mpgc/rpg127083/model/animals/Hare.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg127083/model/animals/Hare.java
@@ -1,13 +1,12 @@
 package it.unicam.cs.mpgc.rpg127083.model.animals;
 
-import it.unicam.cs.mpgc.rpg127083.model.Nest;
 import it.unicam.cs.mpgc.rpg127083.model.challenge.Challenge;
 import it.unicam.cs.mpgc.rpg127083.model.habitats.Habitat;
 
 
 public class Hare extends Animal{
-    public Hare(long id, AnimalType type, Habitat habitat, double life, double energy, double stamina, Nest nest, Challenge currentChallenge) {
-        super(id, type, habitat, life, energy, stamina, nest, currentChallenge);
+    public Hare(long id, AnimalType type, Habitat habitat, double life, double energy, double stamina, Challenge currentChallenge) {
+        super(id, type, habitat, life, energy, stamina, currentChallenge);
     }
 
 }
