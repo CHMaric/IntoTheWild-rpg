def3f8eb456ac568e660a118d0cbe3ba66d7c6ef
Chiara Marinucci <chiaramarinucci94@gmail.com>
2026-06-28 02:19:46 +0200

    added code to challenges

--- app/src/main/java/it/unicam/cs/mpgc/rpg127083/model/challenge/FirstChallenge.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg127083/model/challenge/FirstChallenge.java
@@ -7,11 +7,18 @@ import java.util.List;
 public class FirstChallenge extends Challenge {
     @Override
     public List<Choice> getChoices() {
-        return List.of();
+        return List.of(
+                new Choice("ACT", "Agisci"),
+                new Choice("WAIT", "Aspetta e osserva")
+        );
     }
 
     @Override
     public String executeChoice(Choice choice, Animal animal) {
-        return "";
+        return switch(choice.getId()) {
+            case "ACT" -> animal.handleActChoice(1);
+            case "WAIT" -> animal.handleWaitChoice(1);
+            default -> "Scelta non valida.";
+        };
     }
 }
--- app/src/main/java/it/unicam/cs/mpgc/rpg127083/model/challenge/SecondChallenge.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg127083/model/challenge/SecondChallenge.java
@@ -5,13 +5,19 @@ import it.unicam.cs.mpgc.rpg127083.model.animals.Animal;
 import java.util.List;
 
 public class SecondChallenge extends Challenge {
-    @Override
     public List<Choice> getChoices() {
-        return List.of();
+        return List.of(
+                new Choice("ACT", "Agisci"),
+                new Choice("WAIT", "Aspetta e osserva")
+        );
     }
 
     @Override
     public String executeChoice(Choice choice, Animal animal) {
-        return "";
+        return switch(choice.getId()) {
+            case "ACT" -> animal.handleActChoice(2);
+            case "WAIT" -> animal.handleWaitChoice(2);
+            default -> "Scelta non valida.";
+        };
     }
 }
